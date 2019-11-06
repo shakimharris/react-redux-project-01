6d24402b04daa5a5fb496fe451778858e8d09992
Shakim Harris <amenraprod@gmail.com>
2019-11-06 14:21:52 -0500

    updated with div tags to prevent adjacent jsx error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import Homepage from './pages/homepage/homepage.component';
-import Shop from './pages/shop';
-import  
+import Homepage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header-component";
 
-function App () {
-  return()
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
